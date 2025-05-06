5a79a7fe962b4037c7fbc0880727415cd4b22b3e
Abram morris <abram.morris62@gmail.com>
2025-05-06 20:54:19 +0300

    Initial Commit

---
+++ ToDo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ToDo
+//
+//  Created by abram on 06/05/2025.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ToDo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ToDo
+//
+//  Created by abram on 06/05/2025.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
